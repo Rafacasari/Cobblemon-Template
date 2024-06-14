6f12e091e1260b206ac1337feb39ec416492e605
Rafa <32463720+Rafacasari@users.noreply.github.com>
2024-06-14 09:27:18 -0300

    ops

--- forge/src/main/kotlin/com/your_name/mod/your_mod_id/forge/ForgeLoader.kt
+++ forge/src/main/kotlin/com/your_name/mod/your_mod_id/forge/ForgeLoader.kt
@@ -2,14 +2,10 @@ package com.your_name.mod.your_mod_id.forge
 
 import com.your_name.mod.your_mod_id.YourMod
 import net.minecraftforge.common.MinecraftForge
-import net.minecraftforge.eventbus.api.IEventBus
 import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 
 @Mod(YourMod.MOD_ID)
 class ForgeLoader {
-
-    private val modBus: IEventBus = FMLJavaModLoadingContext.get().modEventBus
 
     init {
         YourMod.initialize()
